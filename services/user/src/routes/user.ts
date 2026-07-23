1c925eaa2aceabd9351f1026eb761bb4f955a5e4
crissiiu <crissiiu.dev@gmail.com>
2026-07-23 14:23:50 +0700

    Feat: User service - Get profile User

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { myProfile } from "../controller/user.js";
+import { getUserProfile, myProfile } from "../controller/user.js";
 import { isAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/me", isAuth, myProfile);
+router.get("/:userId", isAuth, getUserProfile);
 
 export default router;
